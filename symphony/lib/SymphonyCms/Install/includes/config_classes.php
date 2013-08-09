51c74cea3c9a40a1efe2d41e2e7912e36d141f11
designermonkey <john@designermonkey.co.uk>
2013-08-09 17:43:18 +0100

    Default config classes

---
+++ symphony/lib/SymphonyCms/Install/includes/config_classes.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'profiler_class'           => '\\SymphonyCms\\Toolkit\\Profiler',
+    'configuration_class'      => '\\SymphonyCms\\Symphony\\Configuration',
+    'log_class'                => '\\SymphonyCms\\Symphony\\Log',
+    'database_class'           => '\\SymphonyCms\\Toolkit\\MySQL',
+    'cookie_class'             => '\\SymphonyCms\\Symphony\\Cookie',
+    'extension_manager_class'  => '\\SymphonyCms\\Toolkit\\ExtensionManager'
+);
