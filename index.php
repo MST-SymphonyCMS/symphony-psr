41f0c1854afac108e9aacd8d6240ca8474b1c044
designermonkey <john@designermonkey.co.uk>
2013-08-09 17:41:53 +0100

    Change the boot methods

--- index.php
+++ index.php
@@ -3,26 +3,19 @@
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/symphony/boot.php';
 
+use \Exception;
+use \SymphonyCms\Symphony;
 use \SymphonyCms\Symphony\Administration;
 use \SymphonyCms\Symphony\Frontend;
 
-/**
- * Choose the render method for Symphony
- *
- * @param  string $mode The render mode
- * @return Administration|Frontend
- */
-function renderer($mode = 'frontend')
-{
-    if (!in_array($mode, array('frontend', 'administration'))) {
-        throw new Exception('Invalid Symphony Renderer mode specified. Must be either "frontend" or "administration".');
-    }
-    return ($mode == 'administration' ? Administration::instance() : Frontend::instance());
-}
+$mode = (isset($_GET['mode']) && strtolower($_GET['mode']) == 'administration' ? '\\SymphonyCms\\Symphony\\Administration' : '\\SymphonyCms\\Symphony\\Frontend');
 
-$renderer = (isset($_GET['mode']) && strtolower($_GET['mode']) == 'administration' ? 'administration' : 'frontend');
+Symphony::set('mode_class', $mode);
+Symphony::initialise();
 
-$output = renderer($renderer)->display(getCurrentPage());
+$App = new ReflectionClass(Symphony::get('mode_class'));
+
+$output = $App::instance()->display(getCurrentPage());
 
 cleanupSessionCookies();
 
